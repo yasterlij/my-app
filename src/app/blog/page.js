cf6783271d3b923bae11d0a2085adb3624c93373
yasterlij <tamratbelayhun1@gmail.com>
2024-03-22 22:20:39 +0300

    update1

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -30,7 +30,7 @@ export default async function BlogPage() {
     const data = await getData()
     const items = data && data.items ? [...data.items] : []
     console.log(items)
-    console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+    //console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
     return <main>
         <h1>Hello Tamrat!</h1>
         <p> Posts:</p>
